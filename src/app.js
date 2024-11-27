eed68f346cfdbe05d4a77dafe15861e9f72f5b57
Ananda Noviesta Wijaya <anandawijayanoviesta@gmail.com>
2024-11-27 15:53:23 +0700

    Update Deployment Test

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ let express = require("express")
 let app = express()
 let cors = require('cors')
 let dotenv = require('dotenv')
+let stakeholderAuthorization = require('./middleware/stakeholderAuthorization')
 dotenv.config()
-// let port = process.env.PORT
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
 
 let authController = require('./auth/auth.controller')
 let productController = require('./product/product.controller')
@@ -11,10 +18,7 @@ let userController = require('./user/user.controller')
 let orderController = require('./order/order.controller')
 let profileController = require('./profile/profile.controller')
 let dashboardController = require('./dashboard/dashboard.controller');
-let stakeholderAuthorization = require('./middleware/stakeholderAuthorization')
 
-app.use(express.json())
-app.use(cors())
 app.use('/api/auth', authController)
 app.use('/api/products', productController)
 app.use('/api/users', stakeholderAuthorization, userController)
@@ -22,12 +26,5 @@ app.use('/api/orders', orderController)
 app.use('/api/profile', profileController)
 app.use('/api/dashboard', dashboardController)
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
-
-// app.listen(port, () => {
-//     console.log(`Example app listening on port http://localhost:${port}`)
-// })
 
 export default app
