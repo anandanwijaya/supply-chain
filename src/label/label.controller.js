3de37a2c2e6fe1ca277b95b06b809102bbd3ea01
Ananda Noviesta Wijaya <anandawijayanoviesta@gmail.com>
2024-12-15 01:57:55 +0700

    Update Label Endpoint 4

--- src/label/label.controller.js
+++ src/label/label.controller.js
@@ -5,8 +5,8 @@ const labelServices = require('./label.services')
 
 router.get('/', supplierAuthorization, async (req, res) => {
     try {
-        const { id } = req.user_id
-        const labels = await labelServices.getLabelByUserId(id)
+        const user_id = req.user_id
+        const labels = await labelServices.getLabelByUserId(user_id)
         res.status(200).send(labels)
     } catch (error) {
         res.status(500).send(error.message)
